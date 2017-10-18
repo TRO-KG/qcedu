83abdf73ea6679a6dcc48deba358d56dfd4853c8
M_Kg <shuang0819@yeah.net>
2017-10-18 13:25:03 +0800

    调整

--- core/admin.inc.php
+++ core/admin.inc.php
@@ -1,6 +1,6 @@
 <?php
 function checkLogined(){
-	if($_SESSION["admin"]["id"]==""){
+	if($_SESSION["admin"]["id"]==""&&$_COOKIE["adminId"]==""){
 		header("location:login.php");
 	}
 }
@@ -8,6 +8,12 @@ function logout(){
 	$_SESSION=array();
 	if(isset($_COOKIE)){
 		setcookie(session_name(),"",time()-1);
+	}
+	if(isset($_COOKIE["adminName"])){
+		setcookie("adminName","",time()-1);
+	}
+	if(isset($_COOKIE["adminId"])){
+		setcookie("adminId","",time()-1);
 	}
 	session_destroy();
 	header("location:login.php");
