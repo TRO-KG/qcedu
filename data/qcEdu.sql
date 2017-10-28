be1bf6e4b1c672f617214cd4837390ecc10baa8e
M_Kg <shuang0819@yeah.net>
2017-10-29 00:21:34 +0800

    添加sql文件

---
+++ data/qcEdu.sql
@@ -0,0 +1,16 @@
+--创建数据库
+CREATE DATABASE IF NOT EXISTS qcedu DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+USE `qcedu`;
+--创建管理员数据表
+DROP TABLE IF EXISTS `qc_admin`;
+CREATE TABLE `qc_admin`(
+`id` tinyint unsigned AUTO_INCREMENT,
+`username` varchar(20) NOT NULL,
+`account` varchar(20) NOT NULL,
+`password` varchar(32) NOT NULL,
+`email` varchar(50),
+`tel` varchar(11) NOT NULL UNIQUE,
+`submission_date` TIMESTAMP,
+PRIMARY KEY ( id ),
+UNIQUE KEY `account` (`account`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
