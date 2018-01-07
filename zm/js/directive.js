2368770532af8587a37206c5817d59bec15cecff
M_Kg <shuang0819@yeah.net>
2018-01-07 17:43:08 +0800

    新建指令js

---
+++ zm/js/directive.js
@@ -0,0 +1,18 @@
+adm.directive('contenteditable', function() {
+    return {
+        restrict: 'A' ,
+        require: 'ngModel',
+        link: function(scope, element, attrs, ctrl) {
+            // 创建编辑器
+            var editor = new wangEditor('editor-trigger');
+            editor.onchange = function () {
+                // 从 onchange 函数中更新数据
+                scope.$apply(function () {
+                    var html = editor.$txt.html();
+                    ctrl.$setViewValue(html);
+                });
+            };
+            editor.create();
+        }
+    };
+});
